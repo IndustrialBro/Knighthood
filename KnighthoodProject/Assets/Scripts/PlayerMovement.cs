45c7f5cb458e8f89a2cec12ea439117de445b083
IndustrialBro <124711765+IndustrialBro@users.noreply.github.com>
2023-09-24 17:47:38 +0200

    Added documentation
    Added documentation for the hand model that I plan to use in the game

--- KnighthoodProject/Assets/Scripts/PlayerMovement.cs
+++ KnighthoodProject/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,22 @@ public class PlayerMovement : MonoBehaviour
     float speed;
     [SerializeField]
     Transform RayOrigin;
+    float horMove;
+    float verMove;
+
+    [Header("Running")]
+    [SerializeField]
+    float runningSpeed;
+    [SerializeField]
+    float maxStamina;
+    float currStamina;
+    [SerializeField]
+    float maxRunningCooldown;
+    float currRunningCooldown = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currStamina = maxStamina;
     }
 
     
@@ -22,12 +36,44 @@ public class PlayerMovement : MonoBehaviour
 
     void WalkAround()
     {
-        float horMove = Input.GetAxisRaw("Horizontal");
-        float verMove = Input.GetAxisRaw("Vertical");
+        horMove = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        verMove = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+        ProcessRunning();
         
         Vector3 localMove = new Vector3(horMove * speed, 0, verMove * speed);
         Vector3 globalMove = transform.TransformDirection(localMove);
         
         rb.velocity = new Vector3(globalMove.x, rb.velocity.y, globalMove.z);
+    }
+
+    void ProcessRunning()
+    {
+        if(currRunningCooldown <= 0)
+        {
+            if(currStamina > 0)
+            {
+                if (isRunning())
+                {
+                    verMove = verMove * runningSpeed;
+                    currStamina -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                currRunningCooldown = maxRunningCooldown;
+            }
+        }
+        else
+        {
+            currRunningCooldown -= Time.deltaTime;
+            currStamina += Time.deltaTime;
+            Debug.Log(currRunningCooldown);
+        }
+    }
+
+    private bool isRunning()
+    {
+        return Input.GetAxisRaw("Run") > 0;
     }
 }
